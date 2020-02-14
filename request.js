2109cece9317d81425f120963a3f11f100030a5d
Dimas Satrio <dimassrio@gmail.com>
2020-02-14 16:49:41 +0700

    fixing package.json

--- request.js
+++ request.js
@@ -1,21 +1,22 @@
 'use strict'
+
 const CoreFilter = require('./filter')
 
-function CoreRequest({model, request}) {
-    if(model.filter){
-        return model.query().filter(request, model.filter)
-    }
-    return model.query().filter(request, CoreFilter)
+function CoreRequest ({ model, request }) {
+	if (model.filter) {
+		return model.query().filter(request, model.filter)
+	}
+	return model.query().filter(request, CoreFilter)
 }
 
-async function CoreValidation({request, rules = null, model = null}){
-    if(!rules){
-        rules = model.getRules()
-    }
+// async function CoreValidation ({ request, rules = null, model = null }) {
+// 	if (!rules) {
+// 		rules = model.getRules()
+// 	}
 
-    const validation = await Validate(request.all(), rules)
+// 	const validation = await Validate(request.all(), rules)
 
-    return validation
-}
+// 	return validation
+// }
 
-module.exports = {CoreRequest}
+module.exports = { CoreRequest }
